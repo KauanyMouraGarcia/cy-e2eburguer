92f21ae1dab367c768cc55d990026f426074550e
KauanyMouraGarcia <kauany.m.garcia@gmail.com>
2025-01-25 17:06:46 -0300

    Refatorando testes de login e criando fixtures

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,36 +1,29 @@
+import loginData from "../fixtures/login.json"
+
 describe('Login', () => {
   beforeEach(() => {
     cy.acessarHome()
   })
 
   it('Login realizado com sucesso', () => {
-    const usuario = {
-      email: 'teste.qa@gmail.com',
-      password: 'Teste@123!'
-    }
+    const usuario = loginData.perfilGestao
     cy.preencherFormLogin(usuario.email, usuario.password)
     cy.submitBtn('Acessar')
     cy.verificarMsgToast('Login realizado com sucesso!')
-
+    cy.verificarUsuarioLogado(usuario.name)
 
 
   });
 
   it('Login com senha inválida', () => {
-    const usuario = {
-      email: 'teste.qa@gmail.com',
-      password: 'Teste123!'
-    }
+    const usuario = { ...loginData.perfilGestao, password: "Teste123" }
     cy.preencherFormLogin(usuario.email, usuario.password)
     cy.submitBtn('Acessar')
     cy.verificarMsgToast('Credenciais inválidas. Verifique seu e-mail e senha.')
   });
 
   it('Login com email inválido', () => {
-    const usuario = {
-      email: 'teste@gmail.com',
-      password: 'Teste@123!'
-    }
+    const usuario = { ...loginData.perfilGestao, email: "gestao@burguer.com.br" }
     cy.preencherFormLogin(usuario.email, usuario.password)
     cy.submitBtn('Acessar')
     cy.verificarMsgToast('Credenciais inválidas. Verifique seu e-mail e senha.')
@@ -41,6 +34,17 @@ describe('Login', () => {
     cy.submitBtn('Acessar')
     cy.verificarMsgErro('O campo de e-mail é obrigatório.')
     cy.verificarMsgErro('O campo de senha é obrigatório.')
+
+  });
+
+  it('Login perfil salão realizado com sucesso e redirecionado para a página com opção de usar o app', () => {
+    const usuario = loginData.perfilSalao
+    cy.preencherFormLogin(usuario.email, usuario.password)
+    cy.submitBtn('Acessar')
+    cy.verificarMsgToast('Acesse através do app.')
+    cy.verificarPagina('/app-info', 'Acesso pelo APP E2E Burguer')
+
+
 
   });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,18 @@ Cypress.Commands.add('verificarMsgErro', (msgEsperada) => {
         .and('contain.text', msgEsperada)
         .and('have.css', 'color', 'rgb(230, 57, 70)')
 })
+
+Cypress.Commands.add('verificarPagina', (rota, tituloPagina) => {
+    cy.url().should('include', `${rota}`)
+    cy.contains('h1', tituloPagina)
+
+})
+
+Cypress.Commands.add('verificarUsuarioLogado', (nome) => {
+    const nomeUsuario = nome.split(' ')[0]
+    cy.get('[data-testid="user-greeting"]')
+        .should('be.visible')
+        .and('contain.text', `Olá, ${nomeUsuario}!`)
+})
+
+
